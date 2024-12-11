587a35c600377c398f45d2ba13e9edf7ffce517a
ValcirPolettini <valcir.polettini@gmail.com>
2024-12-11 19:56:46 -0300

    aula 1

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,37 +1,28 @@
-import { Stack } from "expo-router"
+import { Stack } from "expo-router";
 import { colors } from "@/styles/theme"
-import { GestureHandlerRootView } from "react-native-gesture-handler"
-
 import {
-  useFonts,
-  Rubik_600SemiBold,
-  Rubik_400Regular,
-  Rubik_500Medium,
-  Rubik_700Bold,
-} from "@expo-google-fonts/rubik"
-
-import { Loading } from "@/components/loading"
-
-export default function Layout() {
-  const [fontsLoaded] = useFonts({
+    useFonts,
     Rubik_600SemiBold,
     Rubik_400Regular,
     Rubik_500Medium,
-    Rubik_700Bold,
-  })
+    Rubik_700Bold
+} from "@expo-google-fonts/rubik"
+import {Loading} from "@/components/loading"
 
-  if (!fontsLoaded) {
-    return <Loading />
-  }
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Rubik_600SemiBold,
+        Rubik_400Regular,
+        Rubik_500Medium,
+        Rubik_700Bold
+    })
 
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: colors.gray[100] },
-        }}
-      />
-    </GestureHandlerRootView>
-  )
+    if (!fontsLoaded){
+        return <Loading/>
+    }
+
+    return <Stack screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.gray[100] },
+    }}/>
 }
--- mobile/src/components/steps/index.tsx
+++ mobile/src/components/steps/index.tsx
@@ -1,31 +1,25 @@
-import { View, Text } from "react-native"
-import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native"
-
-import { s } from "./styles"
+import {View, Text} from "react-native"
+import { IconMapPin,IconQrcode, IconTicket } from "@tabler/icons-react-native"
+import {s} from "./style"
 import { Step } from "../step"
 
 export function Steps() {
-  return (
-    <View style={s.container}>
-      <Text style={s.title}>Veja como funciona:</Text>
-
-      <Step
-        icon={IconMapPin}
-        title="Encontre estabelecimentos"
-        description="Veja locais perto de você que são parceiros Nearby"
-      />
-
-      <Step
-        icon={IconQrcode}
-        title="Ative o cupom com QR Code"
-        description="Escaneie o código no estabelecimento para usar o benefício"
-      />
-
-      <Step
-        icon={IconTicket}
-        title="Garanta vantagens perto de você"
-        description="Ative cupons onde estiver, em diferentes tipos de estabelecimento"
-      />
-    </View>
-  )
+    return (
+        <View style={s.container}>
+            <Text style={s.title}> Veja com funciona:</Text>
+            <Step
+                icon={IconMapPin} 
+                title="teste"
+                description="hueheheu"/>
+            <Step
+                icon={IconQrcode} 
+                title="teste"
+                description="hueheheu"/>
+            <Step
+                icon={IconTicket} 
+                title="teste"
+                description="hueheheu"/>
+            
+        </View>
+    )
 }
